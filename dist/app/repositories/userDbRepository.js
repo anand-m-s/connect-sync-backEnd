1380a9197b206e74c57ff1c86b89411ad18bc2e1
Anand M S <125437178+anand-m-s@users.noreply.github.com>
2024-07-18 16:06:30 +0530

    Delete dist directory

--- dist/app/repositories/userDbRepository.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=userDbRepository.js.map
--- dist/utils/sessionCleanUp.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.cleanupSessionData = void 0;
-const cleanupSessionData = (req) => {
-    const sessionData = req.session;
-    delete sessionData.otp;
-    delete sessionData.otpGeneratedTime;
-};
-exports.cleanupSessionData = cleanupSessionData;
